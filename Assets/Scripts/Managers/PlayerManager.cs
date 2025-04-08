ab03c6729ee60289cc41c602a485fc7ee269d827
Lawice <129510417+Lawice@users.noreply.github.com>
2025-04-08 15:40:39 +0200

    Remove Player

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -1,26 +1,50 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Managers {
     public class PlayerManager : MonoBehaviour {
-        public List<StPlayer> Players = new ();
+        public List<StPlayer> Players = new();
+
+        private void Start() {
+            InputSystem.onDeviceChange += OnDeviceChange;
+        }
 
         public void AddPlayer(PlayerInput player) {
             string name = "Player " + (Players.Count + 1);
+            InputDevice device = player.user.pairedDevices.First();
             player.gameObject.name = name;
-            Players.Add(new StPlayer { Name = name, PlayerInput = player, PlayerObject = player.gameObject });
+            
+            Players.Add(new StPlayer
+                { Name = name, PlayerInput = player, PlayerObject = player.gameObject, Device = device });
         }
-        
-        
-        public void RemovePlayer(PlayerInput player) {
-            Debug.Log("AAAAAAAAAAAAA");
-            StPlayer? playerToRemove = Players.Find(_player => _player.PlayerInput == player);
-            Players.Remove(playerToRemove.Value);
+
+        void OnDeviceChange(InputDevice device, InputDeviceChange change) {
+            if (change != InputDeviceChange.Disconnected) return;
+            foreach (StPlayer player in Players.Where(_player => _player.Device == device)) {
+                RemovePlayer(player);
+                break;
+            }
         }
-        
-        
+
+        public void RemovePlayer(StPlayer player) {
+            Players.Remove(player);
+            RefreshPlayers();
+            if (player.PlayerObject != null) {
+                Destroy(player.PlayerObject);
+            }
+        }
+
+        private void RefreshPlayers() {
+            for (int i = 0; i < Players.Count; i++) {
+                StPlayer player = Players[i];
+                player.Name = $"Player {i + 1}";
+                Players[i].PlayerObject.name = player.Name;
+                Players[i] = player;
+            }
+        }
     }
 
     [Serializable]
@@ -28,5 +52,6 @@ namespace Managers {
         public string Name;
         public PlayerInput PlayerInput;
         public GameObject PlayerObject;
+        public InputDevice Device;
     }
 }

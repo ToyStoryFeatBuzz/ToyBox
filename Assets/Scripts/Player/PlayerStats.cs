cd06f220e3e12825c25e8777c0dc0f514c6aa993
Terrenoss <thieryethan8@gmail.com>
2025-04-09 10:47:46 +0200

    Add PlayerStats

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToyBox.Player
+{
+    public class PlayerStats : MonoBehaviour
+    {
+        
+        
+        public int Score {get;  private set; }
+        
+        public List<int> MatchScores = new();
+
+        public void AddScore(int amount)
+        {
+            Score += amount;
+        }
+        
+        public void RemoveScore(int amount)
+        {
+            Score -= amount;
+        }
+        
+        public void ResetScore()
+        {
+            Score = 0;
+        }
+        
+        public void AddScoreToList(int amount)
+        {
+            MatchScores.Add(amount);
+        }
+    }
+}
+

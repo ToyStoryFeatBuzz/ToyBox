a85fd13ec7b3ef5799ca075015d2473437a548b0
Lawice <129510417+Lawice@users.noreply.github.com>
2025-04-10 16:53:48 +0200

    DevButton

---
+++ Assets/Scripts/Menu/DevButton.cs
@@ -0,0 +1,23 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Toybox.Menu{
+    public class ScDevButton : MonoBehaviour {
+
+        [SerializeField] private SoDevInfo _devInfo;
+
+        [SerializeField] TextMeshProUGUI _name;
+        [SerializeField] Image _image;
+
+        private void Start() {
+            _name.text = _devInfo.Name + "/n" + _devInfo.Surname;
+            _image.sprite = _devInfo.Avatar;
+        }
+
+        public void OnClick() {
+            Application.OpenURL(_devInfo.ItchioUrl);
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/SoDevInfo.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoDevInfo", menuName = "Scriptable Objects/SoDevInfo")]
+public class SoDevInfo : ScriptableObject {
+    public string Name;
+    public string Surname;
+    public string ItchioUrl;
+    
+    public Sprite Avatar;
+}

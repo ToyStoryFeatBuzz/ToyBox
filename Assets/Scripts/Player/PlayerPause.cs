a8905f458f3f880363e8155cdbd21084a4cba710
Lawice <129510417+Lawice@users.noreply.github.com>
2025-04-14 16:42:32 +0200

    DeadPlayer can't Pause

--- Assets/Scripts/Player/PlayerPause.cs
+++ Assets/Scripts/Player/PlayerPause.cs
@@ -5,12 +5,16 @@ using ToyBox.Managers;
 namespace ToyBox.Player {
     public class PlayerPause : MonoBehaviour {
         PauseManager _pauseManager => PauseManager.Instance;
-        
+        private PlayerEnd _playerEnd;
         private void Start() {
             GetComponent<PlayerInputSystem>().OnPauseEvent.Started += StartPause;
+            _playerEnd = GetComponent<PlayerEnd>();
         }
         
         void StartPause() {
+            if (_playerEnd.IsDead) {
+                return;
+            }
             _pauseManager.StartPause(gameObject);
         }
         

54e22f56a616491c87c7d6c78486b43125138545
Terrenoss <thieryethan8@gmail.com>
2025-04-15 16:17:24 +0200

    Fix timer Death when timer = 0

--- Assets/Scripts/Timer/Timer.cs
+++ Assets/Scripts/Timer/Timer.cs
@@ -1,15 +1,19 @@
+using System.Collections.Generic;
 using TMPro;
 using ToyBox.Managers;
+using ToyBox.Player;
 using UnityEngine;
 
-namespace ToyBox.Timer
-{
+namespace ToyBox.Timer {
     public class Timer : MonoBehaviour
     {
         [SerializeField] TextMeshProUGUI _timerText;
-
         GameModeManager _gameModeManager;
-
+        
+        [Header("Player Manager")]
+        [SerializeField] PlayerManager _playerManager;
+        private List<Managers.Player> _listPlayers;
+        
         float _time;
         bool _running;
 
@@ -48,9 +52,20 @@ namespace ToyBox.Timer
             {
                 _time += Time.deltaTime;
                 _timerText.text = _time.ToString("00:00<style=\"Smaller\">.00</style>");
+            } else
+            {
+                KillPlayers();
             }
         }
-
-
+        
+        void KillPlayers()
+        {
+            _listPlayers = _playerManager.GetAlivePlayers();
+            
+            foreach (Managers.Player player in _listPlayers)
+            {
+                player.PlayerObject.GetComponent<PlayerEnd>().SetDeath();
+            }
+        }
     }
 }

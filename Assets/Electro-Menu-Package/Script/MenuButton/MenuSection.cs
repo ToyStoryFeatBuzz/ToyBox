1a0f9eed2ef85b46d9ed7941ea40f3574788aac7
ElectroSpeed <150343183+ElectroSpeed@users.noreply.github.com>
2025-04-21 15:55:25 +0200

    Revert "Feat f inal level disign"

--- Assets/Electro-Menu-Package/Script/MenuButton/MenuSection.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MenuSection : MonoBehaviour
-{
-    public List<GameObject> _menuSectionToDisabled;
-    public List<GameObject> _menuSectionToActivated;
-}

ae71df4e60c7abb62574cdf1a5bed2228750bc22
Lawice <129510417+Lawice@users.noreply.github.com>
2025-04-24 18:35:46 +0200

    Update Player + CountDown Part 1

---
+++ Assets/Scripts/Managers/CountDown.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ToyBox.Managers {
+    public class CountDown : MonoBehaviour {
+        private Image _image;
+        [SerializeField] private List<Sprite> _sprites;
+
+        private void Start() {
+            _image = GetComponent<Image>();
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/LevelKit.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace ToyBox.Managers {
+    public class LevelKit : MonoBehaviour {
+        [SerializeField] private GameObject _UI;
+        [SerializeField] private CountDown _countDown;
+    }
+}

625382ecf119614068278a28d1786b685e91318b
Lawice <129510417+Lawice@users.noreply.github.com>
2025-04-24 20:21:29 +0200

    UI in game

--- Assets/Scripts/Managers/CountDown.cs
+++ Assets/Scripts/Managers/CountDown.cs
@@ -5,11 +5,28 @@ using UnityEngine.UI;
 
 namespace ToyBox.Managers {
     public class CountDown : MonoBehaviour {
-        private Image _image;
+        [SerializeField] private Image _image;
         [SerializeField] private List<Sprite> _sprites;
 
         private void Start() {
             _image = GetComponent<Image>();
+        }
+
+        public void ToggleImage(bool toggle) {
+            Debug.Log("Toggle" + toggle);
+            _image.enabled = toggle;
+        }
+        
+        public void SetSprites(int index) {
+            if (index > _sprites.Count - 1) {
+                index = _sprites.Count-1;
+            }
+
+            if (index < 0) {
+                index = 0;
+            }
+
+            _image.sprite = _sprites[index];
         }
     }
 }
--- Assets/Scripts/Managers/LevelKit.cs
+++ Assets/Scripts/Managers/LevelKit.cs
@@ -1,8 +1,17 @@
+using System;
 using UnityEngine;
 
 namespace ToyBox.Managers {
     public class LevelKit : MonoBehaviour {
         [SerializeField] private GameObject _UI;
-        [SerializeField] private CountDown _countDown;
+        public static LevelKit Instance { get; set; }
+
+        public void ToggleUI(bool toggle) => _UI.SetActive(toggle);
+        
+        private void Awake() {
+            if (Instance == null) {
+                Instance = this;
+            }
+        }
     }
 }
